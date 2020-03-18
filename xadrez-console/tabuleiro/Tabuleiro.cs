bf4f18484b939fe77f2c00b4e9a3ef163bd0b26d
Paulo Henrique <camargopaulohenrique2@gmail.com>
2020-03-18 12:38:08 -0300

    Tratando erros com execoes personalizadas.

--- xadrez-console/tabuleiro/Tabuleiro.cs
+++ xadrez-console/tabuleiro/Tabuleiro.cs
@@ -18,10 +18,42 @@
 			return Pecas[linha, coluna];
 		}
 
+		public Peca peca(Posicao pos)
+		{
+			return Pecas[pos.Linha, pos.Coluna];
+		}
+
+		public bool ExistePeca(Posicao pos)
+		{
+			validarPosicao(pos);
+			return peca(pos) != null;
+		}
+
 		public void colocarPeca(Peca p, Posicao pos)
 		{
+			if (ExistePeca(pos))
+			{
+				throw new TabuleiroException("Já existe uma peca nessa posição !");
+			}
 			Pecas[pos.Linha, pos.Coluna] = p;
 			p.Posicao = pos;
+		}
+
+		public bool posicaoValida(Posicao pos)
+		{
+			if(pos.Linha < 0 || pos.Linha >= Linhas || pos.Coluna < 0 || pos.Coluna>= Colunas)
+			{
+				return false;
+			}
+			return true;
+		}
+
+		public void validarPosicao(Posicao pos)
+		{
+			if (!posicaoValida(pos))
+			{
+				throw new TabuleiroException("Posição inválida !");
+			}
 		}
 	}
 }
